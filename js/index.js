7296b815ddf1f7449926c4bbf59d2741650cf51d
TaiwoAkanbi <akanbitaiwoadetoun@gmail.com>
2026-03-11 12:49:12 +0100

    updated
    i updated the html, css and javascript

--- js/index.js
+++ js/index.js
@@ -4,3 +4,21 @@ let rightLinks = document.querySelector(".right-links");
 menuIcon.addEventListener("click", function () {
   rightLinks.classList.toggle("open-icon");
 });
+
+let links = document.querySelectorAll(".nav-link");
+
+links.forEach((link) => {
+  link.onclick = function () {
+    links.forEach((l) => l.classList.remove("clicked"));
+
+    this.classList.add("clicked");
+  };
+});
+
+const form = document.querySelector("form");
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  alert("Thank you! Your message has been received.");
+  form.reset();
+});
